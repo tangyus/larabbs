96a6839f3a1fbd9b57eb1dc4f3e691411e5f52ed
vagrant <vagrant@homestead>
2017-11-16 14:27:13 +0000

    User authorized

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,18 +9,41 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth', ['except' => ['show']]);
+	}
+	/**
+	 * 查看用户信息
+	 * @param User $user
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
+	/**
+	 * 编辑用户
+	 * @param User $user
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
     public function edit(User $user)
     {
+		$this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
+	/**
+	 * 更新信息
+	 * @param UserRequest $request
+	 * @param ImageUploadHandler $uploader
+	 * @param User $user
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+		$this->authorize('update', $user);
 		$data = $request->all();
 
 		// 处理用户上传头像
