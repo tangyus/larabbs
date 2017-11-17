c3e295d8cca5fe70d551f4f749181eeb3f41fdb0
vagrant <vagrant@homestead>
2017-11-17 19:13:09 +0000

    XSS injection XSS防御攻击

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+
 		Intervention\Image\ImageServiceProvider::class,
 
         /*
@@ -226,7 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-		'Image' => Intervention\Image\Facades\Image::class
+
+		'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
