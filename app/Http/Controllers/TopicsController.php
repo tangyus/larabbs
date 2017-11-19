581f1400798670a6da56b5a83a68724dc18b2461
vagrant <vagrant@homestead>
2017-11-19 14:11:17 +0000

    Add redis queue

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -67,7 +67,7 @@ class TopicsController extends Controller
 		$topic->user_id = Auth::id();
 		$topic->save();
 
-		return redirect()->to($topic->link())->with('message', '创建话题成功！');
+		return redirect()->to($topic->link())->with('success', '创建话题成功！');
 	}
 
 	/**
@@ -93,7 +93,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->to($topic->link())->with('message', '修改话题成功！');
+		return redirect()->to($topic->link())->with('success', '修改话题成功！');
 	}
 
 	/**
@@ -106,7 +106,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', '删除话题成功!');
+		return redirect()->route('topics.index')->with('success', '删除话题成功!');
 	}
 
 	/**
