ea595ffd5abc27c070ccb580e97e094489b8aa42
vagrant <vagrant@homestead>
2017-11-23 13:59:42 +0000

    增加站长和管理员的权限，并测试

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -30,8 +30,8 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @guest
-                <li><a href="{{ route('login') }}">登录</a></li>
-                <li><a href="{{ route('register') }}">注册</a></li>
+                    <li><a href="{{ route('login') }}">登录</a></li>
+                    <li><a href="{{ route('register') }}">注册</a></li>
                 @else
                     <li>
                         <a href="{{ route('topics.create') }}">
@@ -48,7 +48,7 @@
                         </a>
                     </li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
                                 <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
                             </span>
@@ -82,7 +82,7 @@
                             </li>
                         </ul>
                     </li>
-                    @endguest
+                @endguest
             </ul>
         </div>
     </div>
